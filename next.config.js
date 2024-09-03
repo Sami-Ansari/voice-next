a6c20a6ce69f1a66b6b7162e416539aaf53aa87c
Sami-Ansari <samiansari8800@gmail.com>
2024-09-03 21:02:15 +0500

    solve

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env:{
+    DEEPGRAM_API_KEY: process.env.DEEPGRAM_API_KEY,
+    NEETS_API_KEY: process.env.NEETS_API_KEY,
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
